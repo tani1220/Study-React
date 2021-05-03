82b3f51cd41e665043e4963feacf165c7beabd3e
tani <berukoko1220-blog@yahoo.co.jp>
2021-05-03 19:55:50 +0900

    🏗

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,16 +1,15 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import styles from "src/styles/Home.module.css";
 
-import { Footer } from "../components/Footer";
-import { Main } from "../components/Main";
-import { Header } from "../components/Header";
+import { Footer } from "src/components/Footer";
+import { Main } from "src/components/Main";
+import { Header } from "src/components/Header";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
         <title>index page</title>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header />
