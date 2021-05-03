c5d2384d16d0ca345b72b9ef11afa6cde3218683
tani <berukoko1220-blog@yahoo.co.jp>
2021-05-03 15:03:45 +0900

    ♻️

--- components/Links.js
+++ components/Links.js
@@ -1,33 +1,41 @@
 import styles from "./Links.module.css";
 
+const ITEMS = [
+  {
+    href: "https://nextjs.org/docs",
+    title: "Documentation \u2192",
+    discription: "Find in-depth information about Next.js features and API.",
+  },
+  {
+    href: "https://nextjs.org/learn",
+    title: "Learn →",
+    discription: "Learn about Next.js in an interactive course with quizzes!",
+  },
+  {
+    href: "https://github.com/vercel/next.js/tree/master/examples",
+    title: "Examples →",
+    discription: "Discover and deploy boilerplate example Next.js projects.",
+  },
+  {
+    href:
+      "https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+    title: "Deploy →",
+    discription:
+      "Instantly deploy your Next.js site to a public URL with Vercel.",
+  },
+];
+
 export function Links() {
   return (
     <div className={styles.grid}>
-      <a href="https://nextjs.org/docs" className={styles.card}>
-        <h3>Documentation &rarr;</h3>
-        <p>Find in-depth information about Next.js features and API.</p>
-      </a>
-
-      <a href="https://nextjs.org/learn" className={styles.card}>
-        <h3>Learn &rarr;</h3>
-        <p>Learn about Next.js in an interactive course with quizzes!</p>
-      </a>
-
-      <a
-        href="https://github.com/vercel/next.js/tree/master/examples"
-        className={styles.card}
-      >
-        <h3>Examples &rarr;</h3>
-        <p>Discover and deploy boilerplate example Next.js projects.</p>
-      </a>
-
-      <a
-        href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        className={styles.card}
-      >
-        <h3>Deploy &rarr;</h3>
-        <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-      </a>
+      {ITEMS.map((items) => {
+        return (
+          <a key={items.href} href={items.href} className={styles.card}>
+            <h3>{items.title}</h3>
+            <p>{items.discription}</p>
+          </a>
+        );
+      })}
     </div>
   );
 }
