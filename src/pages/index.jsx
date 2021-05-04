d3f19f75e74dc61fe81816388e94537bcd00c7e3
tani <berukoko1220-blog@yahoo.co.jp>
2021-05-04 22:13:58 +0900

    ⚗

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,8 +4,14 @@ import styles from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
+import { useCallback } from "react";
 
 export default function Home() {
+  const hundleClick = useCallback((e) => {
+    console.log(e.target);
+    e.preventDefault();
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,6 +19,10 @@ export default function Home() {
       </Head>
 
       <Header />
+
+      <a href="/about" onClick={hundleClick}>
+        ボタン
+      </a>
 
       <Main page={"index"} />
 
