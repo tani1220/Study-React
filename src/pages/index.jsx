fbe9486574650926d1f6e2e07e42b195bf407c17
tani <berukoko1220-blog@yahoo.co.jp>
2021-05-05 16:17:29 +0900

    ✨

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,12 +4,21 @@ import styles from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 
 export default function Home() {
   const hundleClick = useCallback((e) => {
     console.log(e.target);
     e.preventDefault();
+  }, []);
+
+  useEffect(() => {
+    document.body.style.backgroundColor = "lightblue";
+    console.log("mount");
+    return () => {
+      document.body.style.backgroundColor = "";
+      console.log("unmount");
+    };
   }, []);
 
   return (
