4fefa640748366b8e195e6b79f767c28fbde84f9
tani <berukoko1220-blog@yahoo.co.jp>
2021-05-08 17:51:12 +0900

    ⚡️

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,22 +4,23 @@ import styles from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export default function Home() {
-  const hundleClick = useCallback((e) => {
-    console.log(e.target);
-    e.preventDefault();
-  }, []);
+  const [foo, setFoo] = useState(1);
+
+  const hundleClick = useCallback(() => {
+    if (foo < 10) {
+      setFoo((foo) => foo + 1);
+    }
+  }, [foo]);
 
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
-    console.log("mount");
     return () => {
       document.body.style.backgroundColor = "";
-      console.log("unmount");
     };
-  }, []);
+  }, [foo]);
 
   return (
     <div className={styles.container}>
@@ -28,10 +29,8 @@ export default function Home() {
       </Head>
 
       <Header />
-
-      <a href="/about" onClick={hundleClick}>
-        ボタン
-      </a>
+      <h1>{foo}</h1>
+      <button onClick={hundleClick}>ボタン</button>
 
       <Main page={"index"} />
 
