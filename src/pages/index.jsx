71badc599b9e2c6c9479c676534be5df96c936e4
tani <berukoko1220-blog@yahoo.co.jp>
2021-08-10 16:38:57 +0900

    ⚰️ 不要のコードを削除

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,36 +1,19 @@
 import Head from "next/head";
 import styles from "src/styles/Home.module.css";
 
-import { Footer } from "src/components/Footer";
-import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
 
-const About = (props) => {
+const Index = () => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>about page</title>
+        <title>Index page</title>
       </Head>
       <Header />
-
-      <input type="text" value={props.text} onChange={props.hundleChange} />
-      <button onClick={props.hundleAdd}>追加</button>
-      <ul>
-        {props.array.map((item) => {
-          return <li key={item}>{item}</li>;
-        })}
-      </ul>
-
-      {props.isShow ? <h1>{props.doubleCount}</h1> : null}
-      <button onClick={props.hundleClick}>ボタン</button>
-      <button onClick={props.hundleCount}>
-        {props.isShow ? "非表示" : "表示"}
-      </button>
-
-      <Main page={"about"} />
-      <Footer />
+      <h1>React Study</h1>
+      <p>JsonPlaceholderのAPIを叩く</p>
     </div>
   );
 };
 
-export default About;
+export default Index;
